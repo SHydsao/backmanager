b87ad578d363d3d02f527a1cd48318449f5b16c7
xhmbangbang <whghaigui@outlook.com>
2020-08-25 21:48:47 +0800

    列表-面包屑-搜索框-表格-获取列表数

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,20 +1,87 @@
 <!--
  * @Author: your name
  * @Date: 2020-08-25 20:42:38
- * @LastEditTime: 2020-08-25 20:42:55
+ * @LastEditTime: 2020-08-25 21:44:51
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Editv
  * @FilePath: \mgt_sys\backmanager\src\components\users\users.vue
 -->
 <template>
-  <div>用户</div>
+  <el-card class="box-card">
+    <!-- 1.面包屑 -->
+    <!-- /首页/用户管理/用户列表 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 2.搜索 -->
+    <el-row class="rowSearch">
+      <el-col>
+        <el-input placeholder="请输入内容" v-model="query" class="inputSearch">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success">添加用户</el-button>
+      </el-col>
+    </el-row>
+    <!-- 3.表格 -->
+    <el-table :data="tableData" style="width: 100%">
+      <!-- type="index" 该列的每个单元格的内容从1开始的序号 -->
+      <el-table-column type="index" label="#" width="60"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="80"></el-table-column>
+      <el-table-column prop="address" label="邮箱"></el-table-column>
+      <el-table-column prop="address" label="电话"></el-table-column>
+      <el-table-column prop="address" label="创建时间"></el-table-column>
+      <el-table-column prop="address" label="用户状态"></el-table-column>
+      <el-table-column prop="address" label="操作"></el-table-column>
+    </el-table>
+    <!-- 4.分页 -->
+  </el-card>
 </template>
 <script>
 export default {
- data: () => ({}),
-  created() {},
-  methods: {}
+ data: () => ({
+   query:"",
+   pagenum:1,
+   pagesize:2,
+    tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }]
+ }),
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    //获取用户列表数据
+    //query 查询参数  可以为空
+    //pagenum  当前页码  不能为空
+    //pagesize  每页显示条数  不能为空
+   async getUserList(){
+     //除了登录以外的API 都需要授权
+     //需要授权的API 必须在请求头中使用 Authorization 字段提供 token 令牌
+     const AUTH_TOKEN = localStorage.getItem('token')
+     this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+    const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
+    console.log(res)
+    }
+  }
 };
 </script>
 <style>
+.box-card {
+  height: 100%;
+}
+.rowSearch {
+  margin-top: 16px;
+}
+.inputSearch {
+  width: 360px;
+}
 </style>
