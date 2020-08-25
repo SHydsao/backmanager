cc2d9d7230fb88f00214d9564c3ae0183e29a3e5
xhmbangbang <whghaigui@outlook.com>
2020-08-25 11:33:07 +0800

    新建登录组件配置路由

---
+++ src/components/login/login.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-08-25 11:27:46
+ * @LastEditTime: 2020-08-25 11:28:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \mgt_sys\backmanager\src\components\login\login.vue
+-->
+<template>
+<div>login</div>
+</template>
+<script>
+export default {
+  data: () => ({}),
+  created () {},
+  methods: {}
+}
+</script>
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 /*
  * @Author: your name
  * @Date: 2020-08-25 10:40:57
- * @LastEditTime: 2020-08-25 11:16:58
+ * @LastEditTime: 2020-08-25 11:31:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \mgt_sys\backmanager\src\router\index.js
  */
 import Vue from 'vue'
 import Router from 'vue-router'
+// import Login from '../components/login/login.vue'
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
